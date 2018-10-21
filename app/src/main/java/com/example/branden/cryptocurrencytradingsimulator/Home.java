28bb43e0a00a3c634b5ce1fb121b3ed7be903c5e
bailey srimoungchanh <srimoungchanh.bailey@gmail.com>
2018-10-21 16:22:26 -0500

    Added potential navigation bar

--- app/src/main/java/com/example/branden/cryptocurrencytradingsimulator/Home.java
+++ app/src/main/java/com/example/branden/cryptocurrencytradingsimulator/Home.java
@@ -1,11 +1,13 @@
 package com.example.branden.cryptocurrencytradingsimulator;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,36 +15,13 @@ public class Home extends AppCompatActivity {
 
     private TextView mTextMessage;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_search:
-                    toaster("Search stuff", 500);
-                    return true;
-                case R.id.navigation_settings:
-                    toaster("Settings page", 500);
-                    return true;
-                case R.id.navigation_trade:
-                    toaster("Trade",500);
-                case R.id.navigation_home:
-                    toaster("I'm Home", 500);
-                    return true;
-            }
-            return false;
-        }
-    };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
         mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        configureNavigationButtons();
     }
 
     public void toaster(String message, int length){
@@ -59,4 +38,29 @@ public class Home extends AppCompatActivity {
         }, length);
     }
 
+    private void configureNavigationButtons(){
+        Button tradeButton = (Button) findViewById(R.id.tradeBtn);
+        tradeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Home.this, Trade.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+            }
+        });
+
+        Button settingsButton = (Button) findViewById(R.id.settingsBtn);
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Home.this, settings.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+            }
+        });
+
+        Button searchButton = (Button) findViewById(R.id.searchBtn);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Home.this, search.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+            }
+        });
+    }
 }
--- app/src/main/java/com/example/branden/cryptocurrencytradingsimulator/Trade.java
+++ app/src/main/java/com/example/branden/cryptocurrencytradingsimulator/Trade.java
@@ -1,10 +1,13 @@
 package com.example.branden.cryptocurrencytradingsimulator;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
@@ -13,29 +16,6 @@ import com.jjoe64.graphview.GraphView;
 public class Trade extends AppCompatActivity {
 
     private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener;
-
-    {
-        mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.navigation_trade:
-                        mTextMessage.setText(R.string.title_trade);
-                        return true;
-                    case R.id.navigation_search:
-                        mTextMessage.setText(R.string.title_search);
-                        return true;
-                /*case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;*/
-                }
-                return false;
-            }
-        };
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,8 +33,32 @@ public class Trade extends AppCompatActivity {
         graph.addSeries(series);
 
         mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        configureNavigationButtons();
     }
 
+    private void configureNavigationButtons(){
+        Button homeButton = (Button) findViewById(R.id.homeBtn);
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Trade.this, Home.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+            }
+        });
+
+        Button settingsButton = (Button) findViewById(R.id.settingsBtn);
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Trade.this, settings.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+            }
+        });
+
+        Button searchButton = (Button) findViewById(R.id.searchBtn);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Trade.this, search.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+            }
+        });
+    }
 }
