f6a397ce865e9baad1f79ce11139384038d1f7aa
bailey srimoungchanh <srimoungchanh.bailey@gmail.com>
2018-10-21 22:39:07 -0500

    Removed bottomnavigationbar library

--- app/src/main/java/com/example/branden/cryptocurrencytradingsimulator/Trade.java
+++ app/src/main/java/com/example/branden/cryptocurrencytradingsimulator/Trade.java
@@ -2,7 +2,6 @@ package com.example.branden.cryptocurrencytradingsimulator;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
